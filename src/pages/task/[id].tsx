cfd080181996d76f3813855ba6b7b41284ca8a00
Pollyany Carvalho <kerleypollyany@gmail.com>
2024-10-22 22:04:58 -0300

    Realizando comentário

--- src/pages/task/[id].tsx
+++ src/pages/task/[id].tsx
@@ -3,9 +3,18 @@ import Head from "next/head";
 import styles from "./styles.module.css";
 
 import { db } from "../../services/firebaseConnection";
-import { doc, collection, query, where, getDoc } from "firebase/firestore";
+import {
+  doc,
+  collection,
+  query,
+  where,
+  getDoc,
+  addDoc,
+} from "firebase/firestore";
 
 import { Textarea } from "../../components/textarea";
+import { useSession } from "next-auth/react";
+import { ChangeEvent, FormEvent, useState } from "react";
 
 interface TaskProps {
   item: {
@@ -18,6 +27,31 @@ interface TaskProps {
 }
 
 export default function Task({ item }: TaskProps) {
+  const { data: session } = useSession();
+  const [input, setInput] = useState("");
+
+  async function handleComment(e: FormEvent) {
+    e.preventDefault();
+
+    if (input === "") return;
+
+    if (!session?.user?.email || !session?.user?.name) return;
+
+    try {
+      const docRef = await addDoc(collection(db, "comments"), {
+        comment: input,
+        created: new Date(),
+        user: session?.user?.email,
+        name: session?.user?.name,
+        taskId: item?.taskId
+      });
+
+      setInput("")
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
   return (
     <div className={styles.container}>
       <Head>
@@ -34,9 +68,17 @@ export default function Task({ item }: TaskProps) {
       <section className={styles.commentsContainer}>
         <h2>Deixar comentário</h2>
 
-        <form>
-          <Textarea placeholder="Digite seu comentário..." />
-          <button className={styles.button}>Enviar comentário</button>
+        <form onSubmit={handleComment}>
+          <Textarea
+            value={input}
+            onChange={(e: ChangeEvent<HTMLTextAreaElement>) =>
+              setInput(e.target.value)
+            }
+            placeholder="Digite seu comentário..."
+          />
+          <button disabled={!session?.user} className={styles.button}>
+            Enviar comentário
+          </button>
         </form>
       </section>
     </div>
