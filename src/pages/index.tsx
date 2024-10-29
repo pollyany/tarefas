6ce24903c5fd071b1c1467c8a5ef482c5467ad7f
Pollyany Carvalho <kerleypollyany@gmail.com>
2024-10-28 22:00:47 -0300

    Criando páginas estáticas

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,17 @@ import styles from "@/styles/Home.module.css";
 import Image from "next/image";
 import heroImg from "../../public/assets/hero.png";
 
-export default function Home() {
+import { db } from "../services/firebaseConnection";
+
+import { collection, getDocs } from "firebase/firestore";
+import { GetStaticProps } from "next";
+
+interface HomeProps {
+  posts: number;
+  comments: number;
+}
+
+export default function Home({ posts, comments }: HomeProps) {
   return (
     <div className={styles.container}>
       <Head>
@@ -25,13 +35,29 @@ export default function Home() {
 
         <div className={styles.infoContent}>
           <section className={styles.box}>
-            <span>+12 posts</span>
+            <span>+{posts} posts</span>
           </section>
           <section className={styles.box}>
-            <span>+90 comentários</span>
+            <span>+{comments} comentários</span>
           </section>
         </div>
       </main>
     </div>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const commentRef = collection(db, "comments");
+  const postRef = collection(db, "tarefas");
+
+  const commentSnapshot = await getDocs(commentRef);
+  const postSnapshot = await getDocs(postRef);
+
+  return {
+    props: {
+      posts: postSnapshot.size || 0,
+      comments: commentSnapshot.size || 0,
+    },
+    revalidate: 60, // seria revalidada a cada 60 segundos
+  };
+};
